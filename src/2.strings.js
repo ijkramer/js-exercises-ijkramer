3a5b0e8187126aeb0f5a4a0772b31ec9272ade2f
ijkramer <kramerij@mail.uc.edu>
2024-05-14 21:41:25 -0400

    finsihed test 2.strings

--- src/2.strings.js
+++ src/2.strings.js
@@ -9,19 +9,25 @@ function reverseString(str) {
    * 
    * Can even be done on one line
    */
-  return ;
+  var chara = [];
+  chara = str.split(``);
+  chara.reverse();
+  return chara.join(``);
+
 }
 
 //capitalize: should return the input in all-caps
 // Example: "this is a string" => "THIS IS A STRING"
 function capitalize(str) {
-  return ;
+
+  return str.toUpperCase() ;
 }
 
 // splitString: should divide a string into substrings and return an array
 // Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
 function splitString(str, splitAt = `,`) {
-  return ;
+  return str.split(splitAt);
+
 }
 module.exports = {
   reverseString,
